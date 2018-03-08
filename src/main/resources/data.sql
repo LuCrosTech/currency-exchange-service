1860f005785065c7b3e17638f6fbd70a59f618ef
Luis Crosby <lcustech@gmail.com>
2018-03-08 14:59:10 -0500

    First working commit with zuul implementation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO exchange_value(id, currency_from, currency_to, conversion_multiple,port)
+values (10001, 'USD', 'PEN', 3.25,0);
+INSERT INTO exchange_value(id, currency_from, currency_to, conversion_multiple,port)
+values (10002, 'EUR', 'PEN', 4.25,0);
+INSERT INTO exchange_value(id, currency_from, currency_to, conversion_multiple,port)
+values (10003, 'AUD', 'PEN', 2.25,0);
